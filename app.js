cd0117468d4e4274e8c0b0125d2dc0da76f8c099
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2026-04-02 16:58:08 +0530

    feat: update server configuration and add fixed window middleware

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const fixedWindowMiddleware = require('./src/middleware/fixedWindow');
+
 const app = express();
 
-const PORT = 3300;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 
 
-app.get('/ping', (req, res) => {
+app.get('/ping', fixedWindowMiddleware, (req, res) => {
   res.json({ status: 'ok'});
-})
+});
 
-app.listen(PORT, () =>{
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
